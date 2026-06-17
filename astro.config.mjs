afd04329f689acf70a938233e2c98718e2a0cdd3
Naweekend <nabeel.weekend@gmail.com>
2026-06-17 10:03:34 +0500

    init

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://mysite.com",
+  devToolbar: {
+    enabled: false,
+  },
+  integrations: [sitemap()],
+  prefetch: true,
+  vite: {
+    ssr: {
+      noExternal: ["smartypants"],
+    },
+    plugins: [tailwindcss()],
+  },
+});
