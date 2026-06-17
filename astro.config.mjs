1fcb0a41df9c1446345cbc55fec990d0458fd434
Naweekend <nabeel.weekend@gmail.com>
2026-06-17 10:17:33 +0500

    trying vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mysite.com",
+  site: "https://secondyearpics.vercel.app",
   devToolbar: {
     enabled: false,
   },
